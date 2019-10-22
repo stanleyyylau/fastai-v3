1ad384e43a4bf5d707559673b00dbdd57fc4188b
Stanley <v-staliu@expedia.com>
2019-10-22 18:17:32 +0800

    add port

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import uvicorn
+import os
 from fastai import *
 from fastai.vision import *
 from io import BytesIO
@@ -66,4 +67,4 @@ async def analyze(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+        uvicorn.run(app=app, host='0.0.0.0', port=os.environ.get("PORT", 5000), log_level="info")
